3a7c156b968f029c8bcb572fb8c7262c12644237
Aleksandr Muravev <muravevkot@gmail.com>
2021-10-30 16:10:09 +0300

    func to methods

--- sandbox/src/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,7 +8,9 @@ public class MyFirstProgram {
     hello("Alex");
 
     Square s = new Square(5);
-    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+    System.out.println(
+      "Площадь квадрата со стороной " + s.l + " = " + s.area()
+    );
 
     Rectangle r = new Rectangle(4, 6);
     System.out.println(
@@ -17,19 +19,11 @@ public class MyFirstProgram {
       " и " +
       r.b +
       " = " +
-      area(r)
+      r.area()
     );
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
-  }
-
-  public static double area(Square s) {
-    return s.l * s.l;
-  }
-
-  public static double area(Rectangle r) {
-    return r.a * r.b;
   }
 }
